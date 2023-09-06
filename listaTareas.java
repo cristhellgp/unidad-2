e5d0af6a17725c97a6ee4ab80f34a45371010488
cristhellgp <143815782+cristhellgp@users.noreply.github.com>
2023-09-06 08:47:20 -0600

    Add files via upload

---
+++ listaTareas.java
@@ -0,0 +1,20 @@
+/**@author Juan El caballo Loco**/
+public class listaTareas {
+     public static void main(String[] args) {
+        
+   
+        String tarea1= "tender la cama";
+        String tarea2= "lavar los trastes";
+        String tarea3= "lavar ropa";
+        String tarea4= "hacer tarea ";
+        String tarea5="hacer la comida";
+        
+     System.out.println("primera tarea a realizar "+tarea1);
+         System.out.println("segunda tarea a realizar "+tarea2);
+         System.out.println("tercera tarea a realizar "+ tarea3);
+         System.out.println("cuarta tarea a realizar "+ tarea4);
+         System.out.println("quinta tera a realizar "+tarea5);
+         
+        
+}        
+}
