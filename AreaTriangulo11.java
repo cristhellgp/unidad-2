0da57bae721c478de611e5000fb105c09453487e
cristhellgp <143815782+cristhellgp@users.noreply.github.com>
2023-09-08 08:51:26 -0600

    Add files via upload

---
+++ AreaTriangulo11.java
@@ -0,0 +1,12 @@
+package areatriangulo;
+
+/**@author Juan El caballo Loco**/
+public class AreaTriangulo11 {
+    public static void main(String[] args) {
+         int base = 5;
+        int altura = 3;
+        int area = (base * altura) / 2;
+        System.out.println("la base del triangulo es " + area);
+    }
+
+}
